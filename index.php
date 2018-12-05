6588b7e0465137cd6bf396c59e68e225a5209892
Zeztron <pateltharsh@gmail.com>
2018-12-05 16:07:16 -0500

    Added header and footer files

--- index.php
+++ index.php
@@ -1,60 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>VideoTube</title>
-</head>
-<body>
-    <div id="pageContainer">
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png" alt="Menu Icon">
-            </button>
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" alt="Logo">
-            </a>
-            <div class="searchBarContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBar" name="term" placeholder="Search">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png" alt="Search">
-                    </button>
-                </form>
-            </div>
-            <div class="rightIcons">
-                <a href="upload.php">
-                    <img src="assets/images/icons/upload.png" alt="Upload" class="upload">
-                </a>
-            </div>
-        </div>
-        <div id="sideNavContainer" style="display: none;">
-
-        </div>
-        <div id="mainSectionContainer">
-            <div id="mainContentContainer">
-                hello everyone
-            </div>
-        </div>
-    </div>
-
-    
+<?php require_once("includes/header.php"); ?>
 
 
+hello everyone
 
 
+<?php require_once("includes/footer.php"); ?>
 
-
-
-    <script
-    src="https://code.jquery.com/jquery-3.3.1.min.js"
-    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="assets/js/commonActions.js"></script>
-</body>
-</html>
+            
