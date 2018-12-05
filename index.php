65344520d337e8b472940e36694f7ef58a80a4ed
Zeztron <pateltharsh@gmail.com>
2018-12-05 13:07:47 -0500

    Added the index and css files

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Sandwiches
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>VideoTube</title>
+</head>
+<body>
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+
+        </div>
+        <div id="sideNavContainer">
+
+        </div>
+        <div id="mainSectionContainer">
+        
+        </div>
+    </div>
+</body>
+</html>
