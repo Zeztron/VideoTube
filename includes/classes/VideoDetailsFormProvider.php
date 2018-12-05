67a855a0183133bb1373a61b5d94b589d14ef59d
Zeztron <pateltharsh@gmail.com>
2018-12-05 16:36:45 -0500

    created title and description inputs

--- includes/classes/VideoDetailsFormProvider.php
+++ includes/classes/VideoDetailsFormProvider.php
@@ -2,15 +2,31 @@
     class VideoDetailsFormProvider {
         public function createUploadForm() {
             $fileInput = $this->createFileInput();
+            $titleInput = $this->createTitleInput();
+            $descriptionInput = $this->createDescriptionInput();
             return "<form action='processing.php' method='POST'>
                         $fileInput
+                        $titleInput
+                        $descriptionInput
                     </form>";
         }
 
         private function createFileInput() {
             return "<div class='form-group'>
                         <label for='videoUpload'>Upload your video</label>
-                        <input type='file' class='form-control-file' id='videoUpload' required>
+                        <input type='file' class='form-control-file' id='videoUpload' name='fileInput' required>
+                    </div>";
+        }
+
+        private function createTitleInput() {
+            return "<div class='form-group'>
+                        <input class='form-control' type='text' name='titleInput' placeholder='Title'>
+                    </div>";
+        }
+
+        private function createDescriptionInput() {
+            return "<div class='form-group'>
+                        <textarea class='form-control' placeholder='Description' name='descriptionInput' id='descriptionArea' rows='3'></textarea>
                     </div>";
         }
     }
